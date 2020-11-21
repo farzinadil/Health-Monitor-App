6545f12e0bb24cbef1325d9eefad2ee5be6af0bf
farzinadil <farzinadil@gmail.com>
2020-11-20 17:10:31 -0800

    added form and tbale to blank page

--- tableapi.js
+++ tableapi.js
@@ -1,10 +1,3 @@
-
-
-
-
-
-window.onload(get());
-
 function get(){
 
   let geturl = 'https://api.sheety.co/698e6b1b78ff165acfb5237f82ebb6ec/log/sheet1';
@@ -12,13 +5,28 @@ function get(){
   .then((response) => response.json())
   .then(json => {
     // Do something with the data
-    console.log(json.sheet1);
-    console.log(json.sheet1[0].date);
-    console.log(json.sheet1.length)
-    var i;
-    for (i=0; i<json.sheet1.length; i++){
-      console.log(json.sheet1[i].date);
-    }
+    //console.log(json.sheet1);
+    //console.log(json.sheet1[0].date);
+    //console.log(json.sheet1.length)
+    var data = json.sheet1;
+    console.log("Table DATA:");
+    console.log(data);
+    var container = document.getElementById('table');
+    var hot = new Handsontable(container, {
+      data: data,
+      rowHeaders: false,
+      colHeaders: true,
+      filters: true,
+      dropdownMenu: false,
+      colHeaders: ['Date', 'Day', 'Activity', 'Weight', 'Time', 'Calorie In', 'Calorie Out', 'Id'],
+      licenseKey: 'non-commercial-and-evaluation'
+  
+    });
+  
+
   });
 
+}
+window.onload = function () {
+    get();
 }
