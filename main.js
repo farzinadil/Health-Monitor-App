ebb4d9af598aab2c19fb9460969d874d8b80171b
farzinadil <farzinadil@gmail.com>
2020-11-30 16:52:05 -0800

    uncessful backend integration

---
+++ main.js
@@ -0,0 +1,39 @@
+const mongourl = 'mongodb+srv://Bao:bao45621@cluster0.oiyzl.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+//const PORT = process.env.PORT || 3000;
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const store = new MongoDBStore({
+    uri: mongourl,
+    collection: 'sessions'
+})
+
+const indexRoute = require('./routes/index');
+const registerRoute = require('./routes/register');
+const tablesRoute = require('./routes/tables');
+const data = require('./routes/data');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+    secret: 'bao123456789',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+}));
+app.use('/index', indexRoute);
+app.use('/register', registerRoute);
+app.use('/tables', tablesRoute);
+app.use('/data', data);
+
+mongoose.connect(mongourl,{useNewUrlParser: true}, {useUnifiedTopology: true})
+    .then(client =>{ console.log("Connection success"); })
+    .catch(err => console.error('ERROR CRASHING', err));
+
+const server = app.listen(3000, () => console.log(`Express server listening on port 3000`));
+
+
+module.exports = app;
---
+++ routes/tables.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const path = require("path"); 
+const User = require('../models/user')
+
+router.get('/', (req, res) => {
+    const user = req.session.user;
+    if (user == undefined) //if not logged in, redirect to login page
+        return res.redirect('/index');
+
+    res.sendFile(path.join(__dirname + '/../tables.html'));
+});
+
+//logout button
+router.post('/Logout', (req, res) => {
+    req.session.destroy(); 
+    
+    res.redirect('/index') 
+})
+
+router.post('/Input', (req, res) => {
+
+    let newUserData = req.session.user.userData;
+    newUserData.date.push(req.body.Date);
+    newUserData.activity.push(req.body.Activity);
+    newUserData.weight.push(req.body.Weight);
+    newUserData.minutes.push(req.body.Minutes);
+    newUserData.caloriesIn.push(req.body.CaloriesIn);
+    newUserData.caloriesOut.push(req.body.CaloriesOut);
+
+    User.findByIdAndUpdate({_id: req.session.user._id}, {userData: req.session.user.userData}, {useFindAndModify:true}, function(err, res) {
+        if (err)
+            console.log('err, ID not found', err);
+        else
+            console.log('successfully updated id');
+    });
+
+    res.redirect('/tables') 
+})
+
+module.exports = router;
